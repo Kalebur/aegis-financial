1fbd9faa985be7817fd2040beedcfe99676f3a7f
James Smith <kaleburdev@gmail.com>
2025-06-25 10:36:48 -0400

    Fetch current weather data for the user's current location

--- src/components/WeatherAndNewsBar/WeatherAndNewsBar.jsx
+++ src/components/WeatherAndNewsBar/WeatherAndNewsBar.jsx
@@ -9,6 +9,10 @@ export const WeatherAndNewsBar = () => {
         const locationResponse = await fetch("http://ip-api.com/json/");
         const locationData = await locationResponse.json();
         setLocation(locationData);
+
+        const weatherURI = `https://api.open-meteo.com/v1/forecast?latitude=${locationData.lat}&longitude=${locationData.lon}&hourly=temperature_2m&current=weather_code,temperature_2m&forecast_days=1&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`;
+        const weatherResponse = await fetch(weatherURI);
+        const weatherData = await weatherResponse.json();
       } catch (e) {
         throw new Error(`Something went wrong: ${e.message}`);
       }
