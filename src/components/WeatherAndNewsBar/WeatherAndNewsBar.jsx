f0559ab3792f35000f639a93850e1bc16d4636bc
James Smith <kaleburdev@gmail.com>
2025-06-25 10:37:54 -0400

    Store fetched weather data in state

--- src/components/WeatherAndNewsBar/WeatherAndNewsBar.jsx
+++ src/components/WeatherAndNewsBar/WeatherAndNewsBar.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 export const WeatherAndNewsBar = () => {
   const [location, setLocation] = useState(null);
+  const [weather, setWeather] = useState(null);
 
   useEffect(() => {
     async function fetchLocation() {
@@ -13,6 +14,7 @@ export const WeatherAndNewsBar = () => {
         const weatherURI = `https://api.open-meteo.com/v1/forecast?latitude=${locationData.lat}&longitude=${locationData.lon}&hourly=temperature_2m&current=weather_code,temperature_2m&forecast_days=1&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`;
         const weatherResponse = await fetch(weatherURI);
         const weatherData = await weatherResponse.json();
+        setWeather(weatherData);
       } catch (e) {
         throw new Error(`Something went wrong: ${e.message}`);
       }
