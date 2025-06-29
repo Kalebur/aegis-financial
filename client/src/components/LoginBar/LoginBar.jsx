3e893f5e67df78c206d37e3bcb0bff4cadc866b1
James Smith <kaleburdev@gmail.com>
2025-06-29 13:58:53 -0400

    Create and style the login widget

--- client/src/components/LoginBar/LoginBar.jsx
+++ client/src/components/LoginBar/LoginBar.jsx
@@ -5,6 +5,17 @@ export const LoginBar = () => {
     <section className="right-sidebar">
       <form action="" className="login-form">
         <p className="widget-title">Log In</p>
+        <label htmlFor="email">E-Mail</label>
+        <input type="email" name="email" id="email" />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+
+        <a href="/" className="btn-action">
+          Log In
+        </a>
+        <a href="/">Forgot Password?</a>
+        <a href="/">Create An Account</a>
       </form>
     </section>
   );
