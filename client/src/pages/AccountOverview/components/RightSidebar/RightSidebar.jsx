c3f18556065e3b76f3614a2792754c16ea3a8eab
James Smith <kaleburdev@gmail.com>
2025-07-06 18:16:32 -0400

    Calculate and display total expenses for the month

--- client/src/pages/AccountOverview/components/RightSidebar/RightSidebar.jsx
+++ client/src/pages/AccountOverview/components/RightSidebar/RightSidebar.jsx
@@ -1,12 +1,36 @@
+import { useState, useEffect } from "react";
 import "./RightSidebar.css";
 
 export const RightSidebar = ({ account }) => {
   const { ficoScore } = account;
+  const [currentBalance, setCurrentBalance] = useState(0);
+  const [totalExpenses, setTotalExpenses] = useState(0);
+  const [totalIncome, setTotalIncome] = useState(0);
+
+  const getTotalExpenses = (transactions) => {
+    const today = new Date();
+    const things = transactions
+      .filter(
+        (txn) =>
+          txn.date.getMonth() === today.getMonth() && txn.type === "debit"
+      )
+      .reduce((acc, txn) => (acc += txn.amount), 0);
+    setTotalExpenses(things);
+  };
+
+  useEffect(() => {
+    getTotalExpenses(account.transactions);
+  }, []);
 
   return (
     <section className="right-accounts-bar">
       <p className="totalIncome"></p>
-      <p className="totalExpenses"></p>
+      <p className="totalExpenses">
+        Total Expenses:
+        <span>
+          {totalExpenses > 0 ? `-$${JSON.stringify(totalExpenses)}` : 0}
+        </span>
+      </p>
       <p className="currentBalance"></p>
       <p className="fico-score">FICO Score: {ficoScore}</p>
     </section>
